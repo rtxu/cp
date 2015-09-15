710d0ea749219409e44986155bb258e9cddba9a1
ruitao.xu <ruitao.xu@alibaba-inc.com>
2015-09-15 14:04:58 +0800

    Introduce blade as building system and add thirdparty/gtest
    Change-Id: I2ee96358cef69998c240bad476e1a58190fe1ed0

---
+++ algorithms/two-sum/solution.cpp
@@ -0,0 +1,25 @@
+
+#include <vector>
+#include <unordered_map>
+
+#include <gtest/gtest.h>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        vector<int> indexes;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (m.find(nums[i]) == m.end()) {
+                // not found result yet
+                m[target-nums[i]] = i;
+            } else {
+                // found result
+                indexes.push_back(m[nums[i]]+1);
+                indexes.push_back(i+1);
+                break;
+            }
+        }
+        return indexes;
+    }
+};
