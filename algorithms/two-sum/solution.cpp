1a6bfee4b5e2bedb6265761f143a18667b519bf2
ruitao.xu <ruitao.xu@alibaba-inc.com>
2015-09-15 14:42:52 +0800

    solution for algorithms/two-sum
    Change-Id: If8c4641a9beb916f561c8627e7b4924bec61490f

--- algorithms/two-sum/solution.cpp
+++ algorithms/two-sum/solution.cpp
@@ -2,14 +2,14 @@
 #include <vector>
 #include <unordered_map>
 
-#include <gtest/gtest.h>
+using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> m;
         vector<int> indexes;
-        for (int i = 0; i < nums.size(); ++i) {
+        for (size_t i = 0; i < nums.size(); ++i) {
             if (m.find(nums[i]) == m.end()) {
                 // not found result yet
                 m[target-nums[i]] = i;
