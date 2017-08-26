d07cbf30792f88081b3afca65ca9f5b1c77fb668
ruitao.xu <xrtgavin@163.com>
2017-08-27 01:43:31 +0800

    tutorial: longest-valid-parentheses

--- longest-valid-parentheses/solution.go
+++ longest-valid-parentheses/solution.go
@@ -4,52 +4,24 @@ import "fmt"
 
 func longestValidParentheses(s string) int {
 	n := len(s)
-	stackTop := make([]int, n+1)
-	for i := 0; i <= n; i++ {
-		stackTop[i] = -1 // invalid
-	}
-	for i := 0; i < n; i++ {
-		if s[i] == '(' {
-			if stackTop[i] == -1 {
-				stackTop[i+1] = 1
-			} else {
-				stackTop[i+1] = stackTop[i] + 1
-			}
-		} else {
-			if stackTop[i] > 0 {
-				stackTop[i+1] = stackTop[i] - 1
-			}
-		}
-	}
-
 	maxLength := 0
-	stackTopMinPos := make([]int, n+1)
-	//fmt.Print("[")
-	for i := 0; i <= n; i++ {
-		stackTopMinPos[i] = -1 // invalid
-		//fmt.Printf("%d, ", stackTop[i])
-	}
-	//fmt.Println("]")
-
-	for i := 0; i <= n; i++ {
-		if stackTop[i] == -1 {
-			if stackTopMinPos[0] != -1 {
-				// leave a complete stack
-				stackTopMinPos[0] = -1
-			}
+	validLeft := make(map[int]int)
+	currentHeight := 0
+	validLeft[currentHeight] = 0
+	for i := 1; i <= n; i++ {
+		v := s[i-1]
+		if v == '(' {
+			currentHeight++
+			validLeft[currentHeight] = i
 		} else {
-			if stackTopMinPos[0] == -1 {
-				// enter a new stack
-				stackTopMinPos[0] = i - 1
-			}
-			if stackTopMinPos[stackTop[i]] == -1 {
-				stackTopMinPos[stackTop[i]] = i
-			} else {
-				if i-stackTopMinPos[stackTop[i]] > maxLength {
-					maxLength = i - stackTopMinPos[stackTop[i]]
-					//fmt.Printf("(%d, %d]\n", stackTopMinPos[stackTop[i]], i)
+			delete(validLeft, currentHeight)
+			currentHeight--
+			if left, exist := validLeft[currentHeight]; exist {
+				if i-left > maxLength {
+					maxLength = i - left
 				}
-				stackTopMinPos[stackTop[i]+1] = -1
+			} else {
+				validLeft[currentHeight] = i
 			}
 		}
 	}
