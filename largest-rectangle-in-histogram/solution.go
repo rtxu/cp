b884be0e1f2f726dd7b63ab3a257ee949e1555b2
ruitao.xu <xrtgavin@163.com>
2017-08-27 22:37:35 +0800

    solution: refine largest-rectangle-in-histogram

--- largest-rectangle-in-histogram/solution.go
+++ largest-rectangle-in-histogram/solution.go
@@ -4,32 +4,36 @@ import (
 	"fmt"
 )
 
-func largestRectangleArea(heights []int) int {
-	n := len(heights)
+func largestRectangleArea(h []int) int {
+	n := len(h)
 	if n == 0 {
 		return 0
 	}
-	var maxArea int
-	var left, right int
-	left = -1
-	for right = 1; right < n && heights[right] >= heights[0]; right++ {
-	}
-	maxArea = (right - left - 1) * heights[0]
+
+	// lessFromLeft[i] means the first pos on the left side of i, meet h[lessFromLeft[i]] < h[i]
+	lessFromLeft := make([]int, n)
+	lessFromRight := make([]int, n)
+	lessFromLeft[0] = -1
+	lessFromRight[n-1] = n
 	for i := 1; i < n; i++ {
-		if heights[i] > heights[i-1] {
-			left = i - 1
-			for right = i + 1; right < n && heights[right] >= heights[i]; right++ {
-			}
-		} else if heights[i] < heights[i-1] {
-			for ; left >= 0 && heights[left] >= heights[i]; left-- {
-			}
-			for ; right < n && heights[right] >= heights[i]; right++ {
-			}
-		} else {
-			// do nothing
+		j := i - 1
+		for j >= 0 && h[j] >= h[i] {
+			j = lessFromLeft[j]
 		}
-		if (right-left-1)*heights[i] > maxArea {
-			maxArea = (right - left - 1) * heights[i]
+		lessFromLeft[i] = j
+	}
+	for i := n - 2; i >= 0; i-- {
+		j := i + 1
+		for j < n && h[j] >= h[i] {
+			j = lessFromRight[j]
+		}
+		lessFromRight[i] = j
+	}
+	var maxArea int
+	for i := 0; i < n; i++ {
+		v := (lessFromRight[i] - lessFromLeft[i] - 1) * h[i]
+		if v > maxArea {
+			maxArea = v
 		}
 	}
 	return maxArea
