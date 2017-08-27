46874d523d35f25b59b5f24c6ca284d83e1b3e88
ruitao.xu <xrtgavin@163.com>
2017-08-27 20:34:19 +0800

    solution: largest-rectangle-in-histogram

--- largest-rectangle-in-histogram/solution.go
+++ largest-rectangle-in-histogram/solution.go
@@ -2,21 +2,34 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func largestRectangleArea(heights []int) int {
 	n := len(heights)
-	// minHeight[i][j] means the minimum height between [i, j)
-	// so area[i][j] = (j-i)*minHeight[i][j]
+	if n == 0 {
+		return 0
+	}
 	var maxArea int
-	for i := 0; i < n; i++ {
-		min := heights[i]
-		for j := i + 1; j <= n; j++ {
-			min = int(math.Min(float64(min), float64(heights[j-1])))
-			if (j-i)*min > maxArea {
-				maxArea = (j - i) * min
+	var left, right int
+	left = -1
+	for right = 1; right < n && heights[right] >= heights[0]; right++ {
+	}
+	maxArea = (right - left - 1) * heights[0]
+	for i := 1; i < n; i++ {
+		if heights[i] > heights[i-1] {
+			left = i - 1
+			for right = i + 1; right < n && heights[right] >= heights[i]; right++ {
 			}
+		} else if heights[i] < heights[i-1] {
+			for ; left >= 0 && heights[left] >= heights[i]; left-- {
+			}
+			for ; right < n && heights[right] >= heights[i]; right++ {
+			}
+		} else {
+			// do nothing
+		}
+		if (right-left-1)*heights[i] > maxArea {
+			maxArea = (right - left - 1) * heights[i]
 		}
 	}
 	return maxArea
@@ -26,5 +39,7 @@ func main() {
 	var h []int
 	var expected int
 	h, expected = []int{2, 1, 5, 6, 2, 3}, 10
+	fmt.Printf("h: %v, expected: %d, actual: %d\n", h, expected, largestRectangleArea(h))
+	h, expected = []int{}, 0
 	fmt.Printf("h: %v, expected: %d, actual: %d\n", h, expected, largestRectangleArea(h))
 }
