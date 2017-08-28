b1f7e71504a5149b5e9ab881a97212a3d1a3d874
ruitao.xu <xrtgavin@163.com>
2017-08-28 22:41:53 +0800

    solution(maximal-rectangle): O(n^3), passed

--- maximal-rectangle/solution.go
+++ maximal-rectangle/solution.go
@@ -8,60 +8,35 @@ func maximalRectangle(matrix [][]byte) int {
 	if n > 0 {
 		m = len(matrix[0])
 	}
-	sum := make([][]int, n+1)
-	for i := 0; i <= n; i++ {
-		sum[i] = make([]int, m+1)
-		for j := 0; j <= m; j++ {
-			if i == 0 || j == 0 {
-				sum[i][j] = 0
-			} else {
-				var delta int
-				if matrix[i-1][j-1] == '0' {
-					delta = 0
-				} else {
-					delta = 1
-				}
-				sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + delta
-			}
-		}
-	}
 
-	area := func(i1, j1, i2, j2 int) int {
-		fullArea := (i2 - i1 + 1) * (j2 - j1 + 1)
-		// fmt.Println("in area()", sum[i2+1][j2+1], sum[i2+1][j1], sum[i1][j2+1], sum[i1][j1])
-		if sum[i2+1][j2+1]-sum[i2+1][j1]-sum[i1][j2+1]+sum[i1][j1] == fullArea {
-			return fullArea
-		} else {
-			return -1
-		}
-	}
-	// fmt.Println(area(1, 2, 2, 4))
-
+	row := make([][]int, n)
 	var maxArea int
-	var max int
-	for i1 := 0; i1 < n; i1++ {
-		max = (n - i1) * m
-		if max <= maxArea {
-			break
-		}
-		for j1 := 0; j1 < m; j1++ {
-			max = (n - i1) * (m - j1)
-			if max <= maxArea {
-				break
-			}
-			for i2 := n - 1; i2 >= i1; i2-- {
-				max = (i2 - i1 + 1) * (m - j1)
-				if max <= maxArea {
-					break
+	for i := 0; i < n; i++ {
+		row[i] = make([]int, m)
+		for j := 0; j < m; j++ {
+			delta := int(matrix[i][j] - '0')
+			if j == 0 {
+				row[i][j] = delta
+			} else {
+				if delta == 1 {
+					row[i][j] = row[i][j-1] + 1
+				} else {
+					row[i][j] = 0
 				}
-				for j2 := m - 1; j2 >= j1; j2-- {
-					max = (i2 - i1 + 1) * (j2 - j1 + 1)
-					if max <= maxArea {
+			}
+
+			if row[i][j] > 0 {
+				width, height := -1, -1
+				for i2 := i; i2 >= 0; i2-- {
+					if width == -1 || row[i2][j] < width {
+						width = row[i2][j]
+					}
+					if width == 0 {
 						break
 					}
-					v := area(i1, j1, i2, j2)
-					if v > maxArea {
-						maxArea = v
+					height = i - i2 + 1
+					if width*height > maxArea {
+						maxArea = width * height
 					}
 				}
 			}
