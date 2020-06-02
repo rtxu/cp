96a91f376d07ac03867c8a46317bce3843a51b27
ruitao.xu <ruitao.xu@aliyun.com>
2020-06-02 18:53:25 +0800

    solution(check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence)

---
+++ check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/solution.go
@@ -0,0 +1,9 @@
+func isPrefixOfWord(sentence string, searchWord string) int {
+    words := strings.Split(sentence, " ")
+    for i := 0; i < len(words); i++ {
+        if strings.HasPrefix(words[i], searchWord) {
+            return i+1
+        }
+    }
+    return -1
+}
