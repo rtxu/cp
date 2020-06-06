fe84c59f5616623223c0ad053a40d343e4369ee7
ruitao.xu <ruitao.xu@aliyun.com>
2020-06-06 15:02:40 +0800

    solution(middle-of-the-linked-list): fast-slow-pointer

---
+++ middle-of-the-linked-list/solution.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+    if head == nil {
+        return nil
+    }
+    slow, fast := head, head
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+    return slow
+}
