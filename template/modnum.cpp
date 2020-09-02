2e541c21b95bcfe1ee94481cc5341d6d76e6fe7b
ruitao.xu <ruitao.xu@aliyun.com>
2020-09-02 10:59:08 +0800

    update template modnum.cpp

--- template/modnum.cpp
+++ template/modnum.cpp
@@ -1,79 +1,83 @@
 template <int MOD_> struct modnum {
-	static constexpr int MOD = MOD_;
-	static_assert(MOD_ > 0, "MOD must be positive");
+    // the following line need c++17
+    // static constexpr int MOD = MOD_;
+    static const int MOD = MOD_;
+    static_assert(MOD_ > 0, "MOD must be positive");
 
 private:
-	using ll = long long;
+    using ll = long long;
 
-	int v;
+    int v;
 
-	static int minv(int a, int m) {
-		a %= m;
-		assert(a);
-		return a == 1 ? 1 : int(m - ll(minv(m, a)) * ll(m) / a);
-	}
+    static int minv(int a, int m) {
+        a %= m;
+        assert(a);
+        return a == 1 ? 1 : int(m - ll(minv(m, a)) * ll(m) / a);
+    }
 
 public:
 
-	modnum() : v(0) {}
-	modnum(ll v_) : v(int(v_ % MOD)) { if (v < 0) v += MOD; }
-	explicit operator int() const { return v; }
-	friend std::ostream& operator << (std::ostream& out, const modnum& n) { return out << int(n); }
-	friend std::istream& operator >> (std::istream& in, modnum& n) { ll v_; in >> v_; n = modnum(v_); return in; }
+    modnum() : v(0) {}
+    modnum(ll v_) : v(int(v_ % MOD)) { if (v < 0) v += MOD; }
+    explicit operator int() const { return v; }
 
-	friend bool operator == (const modnum& a, const modnum& b) { return a.v == b.v; }
-	friend bool operator != (const modnum& a, const modnum& b) { return a.v != b.v; }
+    modnum inv() const {
+        modnum res;
+        res.v = minv(v, MOD);
+        return res;
+    }
+    modnum neg() const {
+        modnum res;
+        res.v = v ? MOD-v : 0;
+        return res;
+    }
 
-	modnum inv() const {
-		modnum res;
-		res.v = minv(v, MOD);
-		return res;
-	}
-	modnum neg() const {
-		modnum res;
-		res.v = v ? MOD-v : 0;
-		return res;
-	}
+    modnum operator- () const {
+        return neg();
+    }
+    modnum operator+ () const {
+        return modnum(*this);
+    }
 
-	modnum operator- () const {
-		return neg();
-	}
-	modnum operator+ () const {
-		return modnum(*this);
-	}
+    modnum& operator ++ () {
+        v ++;
+        if (v == MOD) v = 0;
+        return *this;
+    }
+    modnum& operator -- () {
+        if (v == 0) v = MOD;
+        v --;
+        return *this;
+    }
+    modnum& operator += (const modnum& o) {
+        v += o.v;
+        if (v >= MOD) v -= MOD;
+        return *this;
+    }
+    modnum& operator -= (const modnum& o) {
+        v -= o.v;
+        if (v < 0) v += MOD;
+        return *this;
+    }
+    modnum& operator *= (const modnum& o) {
+        v = int(ll(v) * ll(o.v) % MOD);
+        return *this;
+    }
+    modnum& operator /= (const modnum& o) {
+        return *this *= o.inv();
+    }
 
-	modnum& operator ++ () {
-		v ++;
-		if (v == MOD) v = 0;
-		return *this;
-	}
-	modnum& operator -- () {
-		if (v == 0) v = MOD;
-		v --;
-		return *this;
-	}
-	modnum& operator += (const modnum& o) {
-		v += o.v;
-		if (v >= MOD) v -= MOD;
-		return *this;
-	}
-	modnum& operator -= (const modnum& o) {
-		v -= o.v;
-		if (v < 0) v += MOD;
-		return *this;
-	}
-	modnum& operator *= (const modnum& o) {
-		v = int(ll(v) * ll(o.v) % MOD);
-		return *this;
-	}
-	modnum& operator /= (const modnum& o) {
-		return *this *= o.inv();
-	}
+    friend std::ostream& operator << (std::ostream& out, const modnum& n) { return out << int(n); }
+    friend std::istream& operator >> (std::istream& in, modnum& n) { ll v_; in >> v_; n = modnum(v_); return in; }
 
-	friend modnum operator ++ (modnum& a, int) { modnum r = a; ++a; return r; }
-	friend modnum operator -- (modnum& a, int) { modnum r = a; --a; return r; }
-	friend modnum operator + (const modnum& a, const modnum& b) { return modnum(a) += b; }
-	friend modnum operator - (const modnum& a, const modnum& b) { return modnum(a) -= b; }
-	friend modnum operator * (const modnum& a, const modnum& b) { return modnum(a) *= b; }
-	friend modnum operator / (const modnum& a, const modnum& b) { return modnum(a) /= b; }
+    friend modnum operator ++ (modnum& a, int) { modnum r = a; ++a; return r; }
+    friend modnum operator -- (modnum& a, int) { modnum r = a; --a; return r; }
+    // 友元的好处：a 或 b 任一类型为 modnum 即可
+    // 如果是成员函数，则 a (即左操作数) 必须是 modnum
+    friend bool operator == (const modnum& a, const modnum& b) { return a.v == b.v; }
+    friend bool operator != (const modnum& a, const modnum& b) { return a.v != b.v; }
+    friend modnum operator + (const modnum& a, const modnum& b) { return modnum(a) += b; }
+    friend modnum operator - (const modnum& a, const modnum& b) { return modnum(a) -= b; }
+    friend modnum operator * (const modnum& a, const modnum& b) { return modnum(a) *= b; }
+    friend modnum operator / (const modnum& a, const modnum& b) { return modnum(a) /= b; }
 };
